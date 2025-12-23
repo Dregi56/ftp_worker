7f29c3d00a7f84204229ce2e5261b46e4889cad6
Dregi56 <79653938+Dregi56@users.noreply.github.com>
2025-12-23 01:52:20 +0100

    Update run.sh

--- lftp_ftp_worker/run.sh
+++ lftp_ftp_worker/run.sh
@@ -1,29 +1,33 @@
-#!/usr/bin/env bash
+#!/usr/bin/with-conten-env bashio
 set -e
 
-LOG_FILE="/config/files/lftp_worker.log"
+# Recupera le opzioni dal config.yaml tramite bashio
+FTP_HOST=$(bashio::config 'ftp_host')
+FTP_USER=$(bashio::config 'ftp_user')
+FTP_PSW=$(bashio::config 'ftp_psw')
 
-FTP_HOST="${FTP_HOST}"
-FTP_USER="${FTP_USER}"
-FTP_PSW="${FTP_PSW}"
+# Percorso log (assicurati che la cartella /config/ sia scrivibile)
+LOG_FILE="/config/lftp_worker.log"
 
-REMOTE_DIR="$1"
-ACTION="$2"
-LOCAL_DIR="$3"
+# Nota: REMOTE_DIR, ACTION e LOCAL_DIR dovrebbero essere passati come argomenti
+# o recuperati anch'essi dal config se sono fissi.
+REMOTE_DIR=$1
+ACTION=$2
+LOCAL_DIR=$3
 
-echo "[$(date)] Avvio lftp worker - azione: ${ACTION}, dir remota: ${REMOTE_DIR}" >> "$LOG_FILE"
+echo "[$(date)] Avvio lftp worker - azione: ${ACTION}" >> "$LOG_FILE"
 
 if [ -z "$REMOTE_DIR" ] || [ -z "$ACTION" ]; then
-  echo "[$(date)] ERRORE: parametri mancanti" >> "$LOG_FILE"
+  echo "[$(date)] ERRORE: parametri mancanti (REMOTE_DIR o ACTION)" >> "$LOG_FILE"
   exit 1
 fi
 
 case "$ACTION" in
   clean_remote)
     lftp -u "$FTP_USER","$FTP_PSW" "$FTP_HOST" <<EOF
-set ftp:list-options -a
+set ftp:ssl-allow no
 cd /public/${REMOTE_DIR}
-rm *
+rm -r *
 bye
 EOF
     ;;
@@ -33,14 +37,12 @@ EOF
       echo "[$(date)] ERRORE: LOCAL_DIR mancante" >> "$LOG_FILE"
       exit 1
     fi
-
     lftp -u "$FTP_USER","$FTP_PSW" "$FTP_HOST" <<EOF
-set ftp:list-options -a
+set ftp:ssl-allow no
 cd /public/${REMOTE_DIR}
 mput ${LOCAL_DIR}/*
 bye
 EOF
-
     rm -f ${LOCAL_DIR}/*
     ;;
   
@@ -50,4 +52,4 @@ EOF
     ;;
 esac
 
-echo "[$(date)] Operazione completata con successo" >> "$LOG_FILE"
+echo "[$(date)] Operazione completata" >> "$LOG_FILE"
