7fc52ed4c905acfa04b1e12c3c9b779f3fdfe6da
Dregi56 <79653938+Dregi56@users.noreply.github.com>
2025-12-23 10:08:14 +0100

    Update run.sh

--- lftp_ftp_worker/run.sh
+++ lftp_ftp_worker/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/with-conten-env bashio
 
-# 1. Recupero credenziali dalla configurazione dell'add-on
+# 1. Recupero credenziali
 FTP_HOST=$(bashio::config 'ftp_host')
 FTP_USER=$(bashio::config 'ftp_user')
 FTP_PSW=$(bashio::config 'ftp_psw')
@@ -8,18 +8,17 @@ FTP_PSW=$(bashio::config 'ftp_psw')
 bashio::log.info "Motore LFTP Universale avviato."
 bashio::log.info "In attesa di comandi tramite stdin (Standard Input)..."
 
-# 2. Ciclo infinito: resta in ascolto dei comandi inviati da HA
+# 2. Ciclo di ascolto
 while read -r input; do
-    # Verifica che l'input non sia vuoto
     if [ ! -z "$input" ]; then
         bashio::log.info "Ricevuto comando: $input"
         
-        # Esecuzione del comando tramite lftp
+        # Esecuzione lftp (Nota: i comandi sotto NON devono avere spazi a sinistra)
         lftp -u "$FTP_USER","$FTP_PSW" "$FTP_HOST" <<EOF
-        set ftp:passive-mode on
-        set ftp:ssl-allow no
-        $input
-        bye
+set ftp:passive-mode on
+set ftp:ssl-allow no
+$input
+bye
 EOF
         
         if [ $? -eq 0 ]; then
