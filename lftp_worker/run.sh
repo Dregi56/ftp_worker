a78d1ac8ea407b663a9c9950ec4fe40065db470e
Dregi56 <79653938+Dregi56@users.noreply.github.com>
2025-12-23 15:57:43 +0100

    Update run.sh

--- lftp_worker/run.sh
+++ lftp_worker/run.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/with-contenv bashio
 
+bashio::log.info "--- MOTORE LFTP AVVIATO ---"
+
+HOST=$(bashio::config 'host')
+USER=$(bashio::config 'username')
+PASS=$(bashio::config 'password')
+
+while read -r CMD; do
+  bashio::log.info "Eseguo: $CMD"
+  lftp -u "${USER},${PASS}" "${HOST}" -e "${CMD}; quit"
+done
