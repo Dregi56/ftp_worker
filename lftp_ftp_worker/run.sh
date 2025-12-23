10200f144ddf5eff399726c349c1d69e58965590
Dregi56 <79653938+Dregi56@users.noreply.github.com>
2025-12-23 09:47:14 +0100

    Update run.sh

--- lftp_ftp_worker/run.sh
+++ lftp_ftp_worker/run.sh
@@ -1,36 +1,37 @@
 #!/usr/bin/with-conten-env bashio
 set -e
 
-# Recupero credenziali base
 FTP_HOST=$(bashio::config 'ftp_host')
 FTP_USER=$(bashio::config 'ftp_user')
 FTP_PSW=$(bashio::config 'ftp_psw')
-BASE_LOCAL="/share/ftp_upload"
+LOCAL_ROOT=$(bashio::config 'ftp_local_dir') # Es: /share/telecamere
 
-# Questi parametri verranno passati dall'automazione o presi dal config
-# Se passati come variabili d'ambiente da HA
-SUB_DIR="${1:-$(bashio::config 'ftp_remote_dir')}"
+bashio::log.info "Avvio Sincronizzazione Settimanale FTP..."
 
-bashio::log.info "Connessione a $FTP_HOST per la cartella: $SUB_DIR"
+# Elenco delle tue cartelle
+DIRECTORIES=("da_sud" "est_piazzola" "est_cortile")
 
-lftp -u "$FTP_USER","$FTP_PSW" "$FTP_HOST" <<EOF
+for DIR in "${DIRECTORIES[@]}"; do
+    bashio::log.info "Elaborazione cartella: $DIR"
+    
+    lftp -u "$FTP_USER","$FTP_PSW" "$FTP_HOST" <<EOF
 set ftp:passive-mode on
 set ftp:ssl-allow no
-set ftp:list-options -a
-
-# 1. Entra nella directory remota
-cd /public/${SUB_DIR}
-
-# 2. PULIZIA: Rimuove tutti i file esistenti (.mp4 e altro)
-bashio::log.info "Pulizia directory remota /public/${SUB_DIR}..."
+# 1. Entra nella cartella remota (creandola se serve)
+mkdir -p /public/$DIR
+cd /public/$DIR
+# 2. Pulisce i file della settimana precedente sul server
+bashio::log.info "Svuoto remoto: /public/$DIR"
 rm -rf *
-
-# 3. UPLOAD: Trasferisce i nuovi file .mp4
-bashio::log.info "Trasferimento file .mp4 da locale..."
-mput ${BASE_LOCAL}/${SUB_DIR}/*.mp4
-
-# 4. CHIUSURA
+# 3. Carica i file mp4 della settimana corrente
+bashio::log.info "Carico nuovi file da $LOCAL_ROOT/$DIR"
+mput $LOCAL_ROOT/$DIR/*.mp4
 bye
 EOF
 
-bashio::log.info "Operazione completata per $SUB_DIR. Scollegato."
+    # 4. Pulisce il locale per la nuova settimana
+    bashio::log.info "Svuoto locale: $LOCAL_ROOT/$DIR"
+    rm -f $LOCAL_ROOT/$DIR/*.mp4
+done
+
+bashio::log.info "Manutenzione settimanale completata con successo!"
