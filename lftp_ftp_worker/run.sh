4d998cc975fa57a4cf59246a4cd103ddfda4437b
Dregi56 <79653938+Dregi56@users.noreply.github.com>
2025-12-23 11:18:18 +0100

    Update run.sh

--- lftp_ftp_worker/run.sh
+++ lftp_ftp_worker/run.sh
@@ -1,1 +1,15 @@
+#!/usr/bin/with-conten-env bashio
 
+# Carica configurazioni
+HOST=$(bashio::config 'host')
+USER=$(bashio::config 'username')
+PASS=$(bashio::config 'password')
+
+bashio::log.info "Avvio LFTP FTP Worker..."
+bashio::log.info "Connessione a: ${HOST}"
+
+# Motore in ascolto
+while read -r input; do
+    bashio::log.info "Esecuzione comando: $input"
+    lftp -u "${USER},${PASS}" "${HOST}" -e "${input}; quit"
+done
