33ac405790fa1d96b881dc7928d5a6956cfcd618
Dregi56 <79653938+Dregi56@users.noreply.github.com>
2025-12-23 11:39:37 +0100

    Update run.sh

--- lftp_ftp_worker/run.sh
+++ lftp_ftp_worker/run.sh
@@ -1,15 +1,15 @@
-#!/usr/bin/env bash
+#!/usr/bin/with-conten-env bashio
 
-# Carica configurazioni
+# Carica configurazioni usando bashio
 HOST=$(bashio::config 'host')
 USER=$(bashio::config 'username')
 PASS=$(bashio::config 'password')
 
-echo "--- TEST AVVIO ---"
-echo "Provo a connettermi a: ${HOST}"
+bashio::log.info "--- MOTORE AVVIATO ---"
+bashio::log.info "Connessione a: ${HOST}"
 
-# Motore in ascolto
+# Motore in ascolto di comandi
 while read -r input; do
-    echo "Esecuzione comando: $input"
+    bashio::log.info "Esecuzione comando: $input"
     lftp -u "${USER},${PASS}" "${HOST}" -e "${input}; quit"
 done
